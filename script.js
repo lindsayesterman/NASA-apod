c8fe10dcdd2406b7037fa7afdab3e7f78748d378
Lindsay Esterman <lindsayesterman@gmail.com>
2020-06-27 13:29:08 -0400

    added design page and fixed problem with no repeating imgs

--- script.js
+++ script.js
@@ -5,37 +5,50 @@ const apiKey = 'r3jILMQZkXic56riXzLfUPJTt2gZIzarkItsVxyp';
 
 const searchURL = 'https://api.nasa.gov/planetary/apod';
 
+let clickCount=0;
 
 function formatQueryParams(params) {
   const queryItems = Object.keys(params)
-    .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+  .map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join('&');
 }
 
 function displayResults(responseJson) {
-
   console.log(responseJson);
-
-$('input[type=text]').val("");
-$('ul > .flex-container > li > div').replaceWith(
+  if (clickCount<2){
+    $('ul > .flex-container > li > div').replaceWith(
       `<ul>
       <li><div class="imgAppended"><a href="template.html"><img src="${responseJson.hdurl}"></a></div></li>
-    </ul>`
-    );
-};
+      </ul>`
+      )}else{
+     $('ul > li > .imgAppended > a > img').replaceWith(
+      `<ul>
+      <li><div class="imgAppended"><a href="template.html"><img src="${responseJson.hdurl}"></a></div></li>
+      </ul>`
+      );
+   }
+   $('input[type=text]').val("");
+   showImage();
+ };
 
 
-function getAPOD(query) {
-  const params = {
-    api_key: apiKey,
-    date: query,
+ function showImage(){
+  $('ul > li > .imgAppended > a > img').click(function(){
+    $(this).show("slow");
+  })};
 
-  };
 
-  const queryString = formatQueryParams(params)
-  const url = searchURL + '?' + queryString;
-  console.log(url);
-  fetch(url)
+
+  function getAPOD(query) {
+    const params = {
+      api_key: apiKey,
+      date: query,
+    };
+
+    const queryString = formatQueryParams(params)
+    const url = searchURL + '?' + queryString;
+    console.log(url);
+    fetch(url)
     .then(response => {
       if (response.ok) {
         return response.json();
@@ -46,15 +59,16 @@ function getAPOD(query) {
     .catch(err => {
       $('#js-error-message').text(`Something went wrong: ${err.message}`);
     });
-}
+  }
 
-function watchForm() {
-  $('form').submit(event => {
-    event.preventDefault();
-    const searchDate = $('#js-search-date').val();
-    getAPOD(searchDate);
-  });
-}
+  function watchForm() {
+    $('form').submit(event => {
+      event.preventDefault();
+      const searchDate = $('#js-search-date').val();
+      getAPOD(searchDate);
+      clickCount++;
+    });
+  }
 
-$(watchForm);
+  $(watchForm);
 
